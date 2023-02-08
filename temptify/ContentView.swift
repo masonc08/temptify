e48994e25bf308a9d3369f0067c4e765dc96e146
Mason <mxmcao@gmail.com>
2023-02-08 16:02:08 -0500

    new cleaner code

--- temptify/ContentView.swift
+++ temptify/ContentView.swift
@@ -1,74 +1,58 @@
-//
-//  ContentView.swift
-//  temptify
-//
-//  Created by mason on 2023-02-08.
-//
-
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
-//    let button: UIButton = {
-//        let button = UIButton()
-//        button.backgroundColor = .white
-//        button.setTitle("Title", for: .normal)
-//        return button
-//    }()
+    @State private var notificationsSent = 0
+    private let center = UNUserNotificationCenter.current()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Temptify")
-        }
-        .padding()
-        Button("Request Authorization", action: handleAuth)
-        Button("Turn on Notification", action: handleNotif)
-//        button
-    }
-    func handleAuth() {
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            Text("Notifications Sent: \(notificationsSent)")
             
-            if let error = error {
-                // Handle the error here.
+            Button(action: {
+                self.requestPermission()
+            }) {
+                Text("Request Permission")
             }
             
-            // Enable or disable features based on the authorization.
+            Button(action: {
+                self.sendNotification()
+            }) {
+                Text("Send Notification")
+            }
         }
     }
-    func handleNotif() {
-        let center = UNUserNotificationCenter.current()
-        let content =
-        UNMutableNotificationContent()
-        content.title = "Don't miss out!"
-        content.body = "Instagram is calling your name right now! Check it out now."
-        content.sound = UNNotificationSound.default
-        content.userInfo = ["app": "Instagram"]
+    
+    private func requestPermission() {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Notification permission granted")
+            } else {
+                print("Notification permission denied")
+            }
+        }
+    }
+    
+    private func sendNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Test Notification"
+        content.body = "This is a test notification"
         
-        //create dateComponents for lunch time
-        var dateComponents = DateComponents()
-        dateComponents.hour = 15
-        dateComponents.minute = 13
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "test", content: content, trigger: trigger)
         
-        // Create the trigger (lunch time every day)
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        
-        // Create the request
-        let request = UNNotificationRequest(identifier: "lunchNotification", content: content, trigger: trigger)
-        
-        // Schedule the notification
         center.add(request) { (error) in
-           if let error = error {
-               print("Error scheduling notification: \(error)")
-           }
+            if error != nil {
+                print("Error sending notification: \(error!.localizedDescription)")
+            } else {
+                self.notificationsSent += 1
+                print("Notification sent successfully")
+            }
         }
     }
-//    body.addSubview(button)
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    private func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        notificationsSent += 1
+        completionHandler()
     }
 }
