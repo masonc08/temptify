f9397d735a23280111e47a1b41187e224dd5dc4f
Mason <mxmcao@gmail.com>
2023-02-08 13:18:51 -0500

    Initial Commit

---
+++ temptify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  temptify
+//
+//  Created by mason on 2023-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ temptify/temptifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  temptifyApp.swift
+//  temptify
+//
+//  Created by mason on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct temptifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
